73123b9893cd25589d48a8f641bdea52dc34c4b3
Polymath <wickprof@gmail.com>
2023-01-15 01:23:55 +0500

    create-factory-contract

---
+++ contracts/Factory.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Exchange.sol";
+
+
+contract Factory {
+    mapping (address => address) public tokenToExchange;
+
+    
+
+    // * Create an Exchange
+    function createExchange(address _tokenAddress) public returns(address) {
+
+        // * Input Validation
+        require(_tokenAddress != address(0), "Invalid Token Address");
+
+        // * Check if Exchange Already Exists
+        require(tokenToExchange[_tokenAddress] == address(0), "Exchange Already Exists");
+
+        // * Create new Exchange
+        // ? This instantiation is similar to instantiation of
+        // ? classes in OOP languages, however, in Solidity, 
+        // ? the new operator will in fact deploy a contract.
+        Exchange exchange = new Exchange(_tokenAddress);
+
+        // * Add Exchange Address to the Mapping
+        tokenToExchange[_tokenAddress] = address(exchange);
+
+        // * Return Newly Created Exchange's Address
+        return address(exchange);
+    }
+
+    function getExchange(address _tokenAddress) public view returns(address) {
+        return tokenToExchange[_tokenAddress];
+    }
+}
